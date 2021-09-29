56d3257b5b71cfcf9cf554be9a85174f723d53db
Jonathan Sanchez <jonathan.sanchez11b@gmail.com>
2021-09-29 14:21:34 -0500

    feat: added solution 2

---
+++ src/Solution2.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Solution2 {
+    
+    public static void main(String[] args){
+
+        int i = 4;
+
+        double d = 4.0;
+
+        String s = "HackerRank ";
+
+        Scanner sc = new Scanner(System.in);
+
+        int i2 = sc.nextInt();
+
+        double d2 = sc.nextDouble();
+
+        
+        String s2 = sc.nextLine();
+    }
+}
