e59de315e4684acf5ee6a7b48f40a304e3433441
Jonathan Sanchez <jonathan.sanchez11b@gmail.com>
2021-09-29 14:30:46 -0500

    feat: added comments to Day 1

--- src/Day1.java
+++ src/Day1.java
@@ -7,14 +7,17 @@ public class Day1 {
 
     public static void main(String[] args){
 
+        //Declare variables to be added to input variables.
         int i = 4;
 
         double d = 4.0;
 
         String s = "HackerRank ";
 
+        //Import Java Scanner
         Scanner sc = new Scanner(System.in);
 
+        //Declare input variables
         int i2 = sc.nextInt();
 
         double d2 = sc.nextDouble();
@@ -23,6 +26,7 @@ public class Day1 {
         String s2 = sc.nextLine();
         sc.nextLine();
 
+        //Print and run to test
         System.out.println(i + i2);
         System.out.println(d + d2);
         System.out.println(s + s2);
