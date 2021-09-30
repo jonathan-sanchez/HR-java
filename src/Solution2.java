00f65dd030b2cffdc996351fe513b501d8c2b522
Jonathan Sanchez <jonathan.sanchez11b@gmail.com>
2021-09-30 17:29:40 -0500

    completed: Solution 2

--- src/Solution2.java
+++ src/Solution2.java
@@ -2,8 +2,21 @@ import java.util.Scanner;
 
 public class Solution2 {
     public static void main(String[] args){
-        
+
+        //Scanner imported here
         Scanner sc = new Scanner(System.in);
-        
+
+        System.out.println("================================");
+
+        //for-loop for i
+        for(int i = 0; i < 3; i++){
+
+            String s1 = sc.next();
+            int x = sc.nextInt();
+
+            //Formatted print statement
+            System.out.printf("%-15s%03d%n", s1, x);
+        }
+        System.out.println("================================");
     }
 }
