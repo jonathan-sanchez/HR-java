c0574faad558ed6b47c2e2a4634209a6288a3457
Jonathan Sanchez <jonathan.sanchez11b@gmail.com>
2021-10-02 10:38:55 -0500

    feat: updated

---
+++ src/Day3.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+public class Day3 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt(); 
+        scan.close();
+        String ans="";
+            
+        // if 'n' is NOT evenly divisible by 2 (i.e.: n is odd)
+        if(n%2==0){
+              if( n >= 2  && n <=5){
+                  ans = "Not Weird";
+              }
+  
+              if( n >= 6  && n <= 20){
+                  ans = "Weird";
+              }
+  
+  
+              if( n > 20){
+                  ans = "Not Weird";
+              }
+        }
+        else{
+           ans = "Weird";
+           // Complete the code 
+        }
+        
+     }
+}
