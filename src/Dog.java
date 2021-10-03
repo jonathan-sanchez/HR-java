4428bcff7022113fd98b12acd6270df989ad4232
Jonathan Sanchez <jonathan.sanchez11b@gmail.com>
2021-10-03 16:59:22 -0500

    added sample instance vs class

---
+++ src/Dog.java
@@ -0,0 +1,13 @@
+public class Dog { // class name
+
+    static String unnamed = "I need a name!"; // class variable
+    int weight; // Instance Variable
+    String name; // Instance Variable
+    String coatColor; // Instance Variable
+
+    Dog() { // Default Constructor
+        this.weight = 0;
+        this.name = unnamed;
+        this.coatColor = "none";
+    }
+}
