cee7e60e63ccf9ac394f2802aeaad4ed48bc4067
Jonathan Sanchez <jonathan.sanchez11b@gmail.com>
2021-10-03 17:32:26 -0500

    completed: sample class vs instances

--- src/Dog.java
+++ src/Dog.java
@@ -1,3 +1,11 @@
+// Class - A blueprint defining the characteristics of an object of that class type.
+
+// Each class has two types of variables: class variables and instance variables
+
+// Class variables point to the same (static) variable across all instances of a class.
+
+// Instance variables have distinct values that vary from instance to instance.
+
 public class Dog { // class name
 
     static String unnamed = "I need a name!"; // class variable
@@ -5,11 +13,19 @@ public class Dog { // class name
     String name; // Instance Variable
     String coatColor; // Instance Variable
 
+    // class constructor - Creates an instance of a class(i.e.: calling the Dog constructor creates an instance of Dog).
+
+    // A class can have one or more constructors that build different versions of the same  type of object.
+
+    // A constructor with no parameters is called a default constructor;  it creates an object with default initial values specified by the programmer.
+
     Dog() { // Default Constructor
         this.weight = 0;
         this.name = unnamed;
         this.coatColor = "none";
     }
+
+    // A constructor that take sone or more parameters(i.e.: values in parenthesis) is called a parameterized constructor.
 
     Dog(int weight, String color) { // Parameterized constructor
 
@@ -17,5 +33,15 @@ public class Dog { // class name
         this.weight = weight; // Assign parameter's value to instance variable
         this.name = unnamed;
         this.coatColor = color;
+    }
+
+    // Many languages allow you to have multiple constructors, provided that each constructor takes different types of parameters; these are called overloaded constructors.
+    
+    Dog(String dogName, String color) { //Overloaded parametized constructor
+        // Initialize instance variables
+        this.weight = 0;
+        this.name = dogName;
+        this.coatColor = color;
+
     }
 }
